dde44cba35d02b7a35a4022661e12e9e28f718e6
Renato <renato.matosinho.martins@gmail.com>
2020-04-21 16:23:16 -0300

    feat: Add IBitfield interface

---
+++ RUDP/Interfaces/IBitfield.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RUDP.Interfaces
+{
+	public interface IBitfield
+	{
+		bool this[int index] { get; set; }
+		void ShiftRight(int amount);
+		void ShiftLeft(int amount);
+		byte[] ToBytes();
+		void FromBytes(byte[] buffer);
+	}
+}
