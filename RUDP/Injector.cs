49d973bff8fb5ddd0f67a741bcb3ea539b1ff241
Renato <renato.matosinho.martins@gmail.com>
2020-04-21 20:52:59 -0300

    feat: Add placeholder factory class

---
+++ RUDP/Injector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RUDP.Interfaces;
+
+namespace RUDP
+{
+	public class Injector
+	{
+		static private Injector _instance;
+		public static Injector Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new Injector();
+				return _instance;
+			}
+
+			set
+			{
+				if (_instance == null)
+					_instance = value;
+				else
+					throw new Exception("Instance is not null");
+			}
+		}
+
+		private Dictionary<Type, Type> _typeDictionary = new Dictionary<Type, Type>()
+		{
+			
+		};
+
+		private T InjectorCreateInstance<T>()
+		{
+			return (T)_typeDictionary[typeof(T)].GetConstructor(new Type[]{ }).Invoke(new object[] { });
+		}
+
+		static public T CreateInstance<T>()
+		{
+			return Instance.InjectorCreateInstance<T>();
+		}
+	}
+}
