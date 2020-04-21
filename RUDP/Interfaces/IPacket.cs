fb2238868b56769e386cb5bf72b650bc63a2a818
Renato <renato.matosinho.martins@gmail.com>
2020-04-21 18:14:12 -0300

    refactor: Move Packet to a separate file

---
+++ RUDP/Interfaces/IPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RUDP.Enumerations;
+
+namespace RUDP.Interfaces
+{
+	public interface IPacket
+	{
+		ushort AppId { get; set; }
+		ushort SequenceNumber { get; set; }
+		ushort AckSequenceNumber { get; set; }
+		IBitfield Bitfield { get; set; }
+		PacketType Type { get; set; }
+		byte[] Data { get; set; }
+		uint Crc32 { get; set; }
+	}
+}
---
+++ RUDP/Packet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RUDP.Enumerations;
+using RUDP.Interfaces;
+
+namespace RUDP
+{
+	public class Packet : IPacket
+	{
+		public ushort AppId { get; set; }
+		public ushort SequenceNumber { get; set; }
+		public ushort AckSequenceNumber { get; set; }
+		public IBitfield Bitfield { get; set; }
+		public PacketType Type { get; set; }
+		public byte[] Data { get; set; }
+		public uint Crc32 { get; set; }
+	}
+}
