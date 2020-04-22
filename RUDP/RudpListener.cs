7193737f8f23264cb0716408e9847ac1c92636b0
Renato <renato.matosinho.martins@gmail.com>
2020-04-22 03:26:01 -0300

    feat: Add RudpListener file

---
+++ RUDP/RudpListener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+using RUDP.Interfaces;
+
+namespace RUDP
+{
+	class RudpListener : IRudpListener
+	{
+		public bool Active => throw new NotImplementedException();
+
+		public bool ExclusiveAddressUse { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public EndPoint LocalEndpoint => throw new NotImplementedException();
+
+		public IRudpPSocket Server => throw new NotImplementedException();
+
+		private IRudpPSocket _socket;
+
+		public IRudpClient AcceptClient()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AllowNatTraversal(bool allowed)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IRudpListener Create(int port)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Pending()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Start()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Start(int backlog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Stop()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ListenerThread()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
