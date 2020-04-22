3ced1810ebb620aa35daaa5d10429ab1bbb8bd94
Renato <renato.matosinho.martins@gmail.com>
2020-04-21 21:00:47 -0300

    feat: Add PacketType enum

---
+++ RUDP/Enumerations/PacketType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RUDP.Enumerations
+{
+	public enum PacketType : ushort
+	{
+		ConnectionRequest,
+		DisconnectionNotify,
+		KeepAlive,
+		Data
+	}
+}
