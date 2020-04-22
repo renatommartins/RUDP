02a8afe508b9e58c8f18d6ca5d7f8a74d077fdb7
Renato <renato.matosinho.martins@gmail.com>
2020-04-22 02:42:21 -0300

    Add RUDPInternalSocket class

---
+++ RUDP/RUDPInternalSocket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+using RUDP.Interfaces;
+
+namespace RUDP
+{
+	internal class RUDPInternalSocket : Socket, IRUDPSocket
+	{
+		public RUDPInternalSocket(SocketInformation socketInformation) : base(socketInformation)
+		{
+		}
+
+		public RUDPInternalSocket(SocketType socketType, ProtocolType protocolType) : base(socketType, protocolType)
+		{
+		}
+
+		public RUDPInternalSocket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType) : base(addressFamily, socketType, protocolType)
+		{
+		}
+
+		ISocket ISocket.Accept()
+		{
+			return (ISocket)Accept();
+		}
+	}
+}
