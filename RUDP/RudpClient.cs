b319539fb616229577887142c45415852743ea00
Renato <renato.matosinho.martins@gmail.com>
2020-05-11 04:23:38 -0300

    feat: Initial implementation for 'RudpListener' and 'RudpClient'

--- RUDP/RudpClient.cs
+++ RUDP/RudpClient.cs
@@ -1,78 +1,364 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Linq;
 
 using RUDP.Interfaces;
 using RUDP.Enumerations;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace RUDP
 {
 	public delegate void SendEventCallback(ushort seqNumber, RudpEvent sendEvent);
 
+	/// <summary>
+	/// Reliable User Datagram Protocol client implementation.
+	/// </summary>
 	public class RudpClient
 	{
-		public bool Active { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		/// <summary>
+		/// States in which a client may be in.
+		/// </summary>
+		private enum State
+		{
+			Disconnected,
+			Connecting,
+			Connected,
+			Disconnecting,
+			ForceClose
+		};
 
-		public int Available => throw new NotImplementedException();
-
-		public IRudpSocket Client { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-		public bool Connected => throw new NotImplementedException();
-
-		public bool ExclusiveAddressUse { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		/// <summary>
+		/// Application Identifier.
+		/// </summary>
+		public ushort AppId { get; private set; }
+		/// <summary>
+		/// Number of packets sent per second.
+		/// </summary>
+		public int SendRate { get; private set; }
+		/// <summary>
+		/// Received byte sequences count.
+		/// </summary>
+		public int Available => _receiveDataQueue.Count;
+		/// <summary>
+		/// Indicates whether this client is connected to a remote host.
+		/// </summary>
+		public bool Connected => _state == State.Connected;
+		/// <summary>
+		/// Gets or Sets whether the socket remains connected until all data is sent.
+		/// </summary>
 		public LingerOption LingerState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public int ReceiveBufferSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public int SendBufferSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		public int UpdateRate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		/// <summary>
+		/// Gets the remote endpoint.
+		/// </summary>
+		public IPEndPoint RemoteEndpoint { get; private set; }
 
+		/// <summary>
+		/// Contains the actual bound socket in server mode.
+		/// </summary>
+		internal RudpListener Listener { get; private set; }
+
+		/// <summary>
+		/// Contains the bound socket in client mode.
+		/// </summary>
 		private IRudpSocket _socket;
+		/// <summary>
+		/// Communication handling thread in client mode.
+		/// </summary>
+		private Thread _clientThread;
+		/// <summary>
+		/// Tracks send delay between packets in client mode.
+		/// </summary>
+		private Stopwatch _sendStopwatch;
+		private Stopwatch _timeoutStopwatch;
+
+		private ushort _nextSeqNumber = 0;
+		private ushort _lastAckSeqNum = 0;
+		private Queue<byte[]> _sendDataQueue = new Queue<byte[]>();
+		private Dictionary<ushort, SendEventCallback> _pendingAckPackets = new Dictionary<ushort, SendEventCallback>();
+
+		private Queue<byte[]> _receiveDataQueue = new Queue<byte[]>();
+		private ushort _lastRemoteSeqNumber = 0;
+		private HashSet<ushort> _remotePacketAcks = new HashSet<ushort>();
+
+		private State _state = State.Disconnected;
+		private bool _isActive = false;
+
+		public RudpClient(ushort appId)
+		{
+			AppId = appId;
+		}
+
+		internal RudpClient(RudpListener listener, IPEndPoint endPoint, ushort seqNumInit)
+		{
+			this.Listener = listener;
+			AppId = Listener.AppId;
+			RemoteEndpoint = endPoint;
+			_nextSeqNumber = seqNumInit;
+
+			_state = State.Connected;
+		}
 
 		public void Close()
 		{
-			throw new NotImplementedException();
+			_isActive = false;
 		}
 
 		public void Connect(IPAddress address, int port)
 		{
-			throw new NotImplementedException();
-		}
-
-		public void Connect(IPAddress[] ipAddresses, int port)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Connect(IPEndPoint remoteEP)
-		{
-			throw new NotImplementedException();
+			Connect(new IPEndPoint(address, port));
 		}
 
 		public void Connect(string hostname, int port)
 		{
-			throw new NotImplementedException();
+			Connect(Dns.GetHostAddresses(hostname)[0], port);
 		}
 
-		public int Receive(byte[] buffer)
+		public void Connect(IPEndPoint remoteEP)
 		{
-			throw new NotImplementedException();
+			RemoteEndpoint = remoteEP;
+			_lastRemoteSeqNumber = ushort.MaxValue;
+			_state = State.Connecting;
+
+			_clientThread = new Thread(new ThreadStart(ClientThread));
+			_clientThread.Start();
+		}
+
+		public byte[] Receive()
+		{
+			lock(_receiveDataQueue)
+				return _receiveDataQueue.Dequeue();
 		}
 
 		public int Send(byte[] buffer)
 		{
-			throw new NotImplementedException();
+			ushort seqNum;
+			return Send(buffer, out seqNum, null);
 		}
 
 		public int Send(byte[] buffer, out ushort seqNumber, SendEventCallback callback)
 		{
-			throw new NotImplementedException();
+			seqNumber = _nextSeqNumber;
+			lock(_sendDataQueue)
+				lock(_pendingAckPackets)
+				{
+					_sendDataQueue.Enqueue(buffer);
+					if (!_pendingAckPackets.ContainsKey(seqNumber))
+						_pendingAckPackets.Add(_nextSeqNumber, callback);
+					else
+						_pendingAckPackets[seqNumber] = callback;
+				}
+
+			return buffer.Length;
+		}
+
+		internal (Packet packet, RudpEvent rudpEvent) SendUpdate()
+		{
+			Packet packet = null;
+			RudpEvent rudpEvent = RudpEvent.Successful;
+
+			if (Packet.SequenceNumberGreaterThan((ushort)(_nextSeqNumber - 1), (ushort)(_lastAckSeqNum + 32)))
+			{
+				packet = new Packet()
+				{
+					AppId = AppId,
+					SequenceNumber = _nextSeqNumber++,
+					AckSequenceNumber = _lastRemoteSeqNumber,
+					AckBitfield = GetReceivedBitfield(),
+					Type = PacketType.DisconnectionNotify,
+				};
+
+				rudpEvent = RudpEvent.Disconnected;
+				_isActive = false;
+				_state = State.Disconnected;
+			}
+			else
+				lock (_sendDataQueue)
+					if (_sendDataQueue.Count > 0)
+					{
+						using (MemoryStream stream = new MemoryStream())
+						{
+							while (_sendDataQueue.Count > 0)
+							{
+								byte[] buffer = _sendDataQueue.Dequeue();
+								stream.Write(buffer, 0, buffer.Length);
+							}
+
+							packet = new Packet()
+							{
+								AppId = AppId,
+								SequenceNumber = _nextSeqNumber++,
+								AckSequenceNumber = _lastRemoteSeqNumber,
+								AckBitfield = GetReceivedBitfield(),
+								Type = PacketType.Data,
+								Data = stream.ToArray(),
+							};
+						}
+					}
+					else
+						packet = GetKeepalivePacket();
+
+			return (packet, rudpEvent);
+		}
+
+		private Bitfield GetReceivedBitfield()
+		{
+			Bitfield bitfield = new Bitfield(4);
+
+			for (ushort i = (ushort)(_lastRemoteSeqNumber-1), j = 0; Packet.SequenceNumberGreaterThan(i, (ushort)(_lastRemoteSeqNumber - 33)); i--, j++)
+				if (_remotePacketAcks.Contains(i))
+					bitfield[j] = true;
+
+			return bitfield;
+		}
+
+		internal void ReceiveUpdate(Packet packet)
+		{
+			if (packet.Validate(AppId, _lastRemoteSeqNumber))
+			{
+				_remotePacketAcks.Add(_lastRemoteSeqNumber);
+				_remotePacketAcks.RemoveWhere(seqNum => !Packet.SequenceNumberGreaterThan(seqNum, (ushort)(packet.SequenceNumber - 33)));
+				_lastRemoteSeqNumber = packet.SequenceNumber;
+
+				lock(_pendingAckPackets)
+				{
+					if (_pendingAckPackets.ContainsKey(packet.AckSequenceNumber))
+					{
+						_pendingAckPackets[packet.AckSequenceNumber]?.Invoke(packet.AckSequenceNumber, RudpEvent.Successful);
+						_pendingAckPackets.Remove(packet.AckSequenceNumber);
+					}
+					for (ushort i = (ushort)(packet.AckSequenceNumber - 1), j = 0; Packet.SequenceNumberGreaterThan(i, (ushort)(packet.AckSequenceNumber - 33)); i--, j++)
+					{
+						if (packet.AckBitfield[j] == true && _pendingAckPackets.ContainsKey(i))
+						{
+							_pendingAckPackets[i]?.Invoke(i, RudpEvent.Successful);
+							_pendingAckPackets.Remove(i);
+						}
+					}
+					foreach (var pair in _pendingAckPackets.Where(seqNum => !Packet.SequenceNumberGreaterThan(seqNum.Key, (ushort)(packet.AckSequenceNumber - 33))).ToList())
+					{
+						_pendingAckPackets[pair.Key]?.Invoke(pair.Key, RudpEvent.Dropped);
+						_pendingAckPackets.Remove(pair.Key);
+					}
+					_lastAckSeqNum = packet.AckSequenceNumber;
+				}
+
+				switch(_state)
+				{
+					case State.Connecting:
+						switch(packet.Type)
+						{
+							case PacketType.ConnectionAccept:
+								SendRate = packet.Data[0];
+								_sendStopwatch = new Stopwatch();
+								_sendStopwatch.Start();
+								_timeoutStopwatch.Stop();
+								_state = State.Connected;
+								break;
+							case PacketType.ConnectionRefuse:
+								_isActive = false;
+								_state = State.Disconnected;
+								break;
+						}
+						break;
+					case State.Connected:
+						switch(packet.Type)
+						{
+							case PacketType.DisconnectionNotify:
+								_isActive = false;
+								_state = State.Disconnected;
+								break;
+							case PacketType.Data:
+								_receiveDataQueue.Enqueue(packet.Data);
+								break;
+						}
+						break;
+					case State.Disconnecting:
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		internal Packet GetKeepalivePacket()
+		{
+			lock (_pendingAckPackets)
+				_pendingAckPackets.Add(_nextSeqNumber, null);
+			return new Packet()
+			{
+				AppId = AppId,
+				SequenceNumber = _nextSeqNumber++,
+				AckSequenceNumber = _lastRemoteSeqNumber,
+				AckBitfield = GetReceivedBitfield(),
+				Type = PacketType.KeepAlive,
+				Data = null,
+			};
 		}
 
 		private void ClientThread()
 		{
-			throw new NotImplementedException();
+			_socket = new RudpInternalSocket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			_socket.Bind(new IPEndPoint(IPAddress.Any, RemoteEndpoint.Port));
+
+			_socket.SendTo(new Packet()
+			{
+				AppId = AppId,
+				SequenceNumber = _nextSeqNumber++,
+				AckSequenceNumber = 0,
+				AckBitfield = new Bitfield(4),
+				Type = PacketType.ConnectionRequest
+			}.ToBytes(),
+			RemoteEndpoint);
+
+			_isActive = true;
+			_timeoutStopwatch = new Stopwatch();
+			_timeoutStopwatch.Start();
+
+			byte[] receiveBuffer = new byte[4096];
+
+			while (_isActive)
+			{
+				if (_timeoutStopwatch.ElapsedMilliseconds > 500)
+				{
+					_isActive = false;
+					_state = State.Disconnected;
+				}
+
+				if (_socket.Available > 0)
+				{
+					EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+					int receiveCount = _socket.ReceiveFrom(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, ref endPoint);
+
+					if(endPoint.Equals(RemoteEndpoint))
+					{
+						Packet packet = new Packet(receiveBuffer, 0, receiveCount);
+						ReceiveUpdate(packet);
+					}					
+				}
+				
+				if(_state == State.Connected)
+					lock(_sendStopwatch)
+						if(_sendStopwatch.ElapsedMilliseconds >= 1000/SendRate)
+						{
+							_sendStopwatch.Restart();
+							(Packet packet, RudpEvent rudpEvent) = SendUpdate();
+							_socket.SendTo(packet.ToBytes(), RemoteEndpoint);
+
+							if (rudpEvent == RudpEvent.Disconnected)
+								break;
+						}
+
+				Thread.Yield();
+			}
+			_sendStopwatch?.Stop();
+			_timeoutStopwatch?.Stop();
+			Close();
 		}
 	}
 }
--- RUDP/RudpListener.cs
+++ RUDP/RudpListener.cs
@@ -1,27 +1,76 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
 
 using RUDP.Interfaces;
+using RUDP.Enumerations;
 
 namespace RUDP
 {
 	public class RudpListener
 	{
-		public bool Active => throw new NotImplementedException();
+		public ushort AppId { get; private set; }
+		public bool Active { get; private set; }
+		public EndPoint LocalEndpoint { get; private set; }
+		public IRudpSocket Server { get; private set; }
+		public int SendRate { get; private set; }
 
-		public bool ExclusiveAddressUse { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		private Thread _listenerThread;
+		private Stopwatch _sendStopwatch;
+		private Dictionary<IPEndPoint, RudpClient> _connectedClients = new Dictionary<IPEndPoint, RudpClient>();
+		private Queue<IPEndPoint> _pendingRequests;
+		private List<(IPEndPoint, Packet)> _acceptReplyList = new List<(IPEndPoint, Packet)>();
+		
+		public RudpListener(ushort appId, int port, int sendRate) : this(appId, new IPEndPoint(IPAddress.Any, port), sendRate) { }
+		
+		public RudpListener(ushort appId, IPAddress address, int port, int sendRate) : this(appId, new IPEndPoint(address, port), sendRate) { }
 
-		public EndPoint LocalEndpoint => throw new NotImplementedException();
-
-		public IRudpPSocket Server => throw new NotImplementedException();
-
-		private IRudpPSocket _socket;
-
-		public IRudpClient AcceptClient()
+		public RudpListener(ushort appId, IPEndPoint endPoint, int sendRate)
 		{
-			throw new NotImplementedException();
+			AppId = appId;
+			LocalEndpoint = endPoint;
+			SendRate = sendRate;
+		}
+
+		public RudpClient AcceptClient()
+		{
+			IPEndPoint endPoint = null;
+			lock (_pendingRequests)
+				if (_pendingRequests.Count > 0)
+					endPoint = _pendingRequests.Dequeue();
+				else
+					return null;
+
+			byte updateRate = (byte)SendRate;
+			byte version = 0;
+
+			Packet acceptPacket = new Packet()
+			{
+				AppId = AppId,
+				SequenceNumber = 0,
+				AckSequenceNumber = 0,
+				AckBitfield = new Bitfield(4),
+				Type = PacketType.ConnectionAccept,
+			};
+
+			using(MemoryStream memoryStream = new MemoryStream())
+			{
+				memoryStream.WriteByte(updateRate);
+				memoryStream.WriteByte(version);
+				acceptPacket.Data = memoryStream.ToArray();
+			}
+
+			RudpClient client = new RudpClient(this, endPoint, 1);
+			_connectedClients.Add(endPoint, client);
+			_acceptReplyList.Add((endPoint, acceptPacket));
+
+			return client;
 		}
 
 		public void AllowNatTraversal(bool allowed)
@@ -29,34 +78,128 @@ namespace RUDP
 			throw new NotImplementedException();
 		}
 
-		public IRudpListener Create(int port)
-		{
-			throw new NotImplementedException();
-		}
-
 		public bool Pending()
 		{
-			throw new NotImplementedException();
+			if (!Active)
+				throw new InvalidOperationException();
+
+			lock(_pendingRequests)
+			{
+				return _pendingRequests.Count > 0;
+			}			
 		}
 
 		public void Start()
 		{
-			throw new NotImplementedException();
+			Start(0);
 		}
 
 		public void Start(int backlog)
 		{
-			throw new NotImplementedException();
+			if (backlog != 0)
+				_pendingRequests = new Queue<IPEndPoint>(backlog);
+			else
+				_pendingRequests = new Queue<IPEndPoint>();
+
+			Active = true;
+
+			_listenerThread = new Thread(new ThreadStart(ListenerThread));
+			_listenerThread.Start();
 		}
 
 		public void Stop()
 		{
-			throw new NotImplementedException();
+			Active = false;
+			lock (_sendStopwatch)
+				_sendStopwatch.Stop();
+
+			lock(_pendingRequests)
+			{
+				_pendingRequests.Clear();
+				_pendingRequests = null;
+			}
 		}
 
 		private void ListenerThread()
 		{
-			throw new NotImplementedException();
+			Server = new RudpInternalSocket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			Server.Bind(LocalEndpoint);
+
+			_sendStopwatch = new Stopwatch();
+			_sendStopwatch.Start();
+
+			byte[] receiveBuffer = new byte[4096];
+
+			while(Active)
+			{
+				if(Server.Available > 0)
+				{
+					EndPoint remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
+					int receiveCount = Server.ReceiveFrom(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, ref remoteEndpoint);
+
+					Packet packet = new Packet(receiveBuffer, 0, receiveCount);
+					if(!_connectedClients.ContainsKey((IPEndPoint)remoteEndpoint))
+					{
+						switch(packet.Type)
+						{
+							case PacketType.ConnectionRequest:
+								{
+									if (packet.Validate(AppId, ushort.MaxValue) && 
+										!_pendingRequests.Contains((IPEndPoint)remoteEndpoint))
+										lock (_pendingRequests)
+											_pendingRequests.Enqueue((IPEndPoint)remoteEndpoint);
+									else
+										Server.SendTo(
+											new Packet()
+											{
+												AppId = AppId,
+												SequenceNumber = 0,
+												AckSequenceNumber = 0,
+												AckBitfield = new Bitfield(4),
+												Type = PacketType.ConnectionRefuse
+											}.ToBytes(),
+											SocketFlags.None,
+											remoteEndpoint
+											);
+								}
+								break;
+							case PacketType.DisconnectionNotify:
+								break;
+							default:
+								break;
+						}
+					}
+					else
+						_connectedClients[(IPEndPoint)remoteEndpoint].ReceiveUpdate(packet);
+					
+				}
+
+				lock (_sendStopwatch)
+					if (_sendStopwatch.ElapsedMilliseconds >= 1000/SendRate)
+					{
+						_sendStopwatch.Restart();
+
+						foreach ((IPEndPoint endpoint, Packet packet) reply in _acceptReplyList)
+							Server.SendTo(reply.packet.ToBytes(), reply.endpoint);
+
+						List<IPEndPoint> disconnectedClients = new List<IPEndPoint>();
+						foreach (var pair in _connectedClients)
+						{
+							(Packet packet, RudpEvent rudpEvent) = pair.Value.SendUpdate();
+
+							if (!pair.Value.Connected)
+								disconnectedClients.Add(pair.Key);
+
+							Server.SendTo(packet.ToBytes(), pair.Key);
+						}
+						foreach (IPEndPoint endPoint in disconnectedClients)
+							_connectedClients.Remove(endPoint);
+
+						_acceptReplyList.Clear();
+					}
+
+				Thread.Yield();
+			}
 		}
 	}
 }
