b48df23337ca17c4caf8532f6186b9c747c27f70
Renato <renato.matosinho.martins@gmail.com>
2020-05-11 23:44:57 -0300

    docs: Add description to 'RudpListener' fields, methods and properties

--- RUDP/RudpListener.cs
+++ RUDP/RudpListener.cs
@@ -13,24 +13,76 @@ using RUDP.Enumerations;
 
 namespace RUDP
 {
+	/// <summary>
+	/// Reliable User Datagram Protocol listener/server implementation.
+	/// </summary>
 	public class RudpListener
 	{
+		/// <summary>
+		/// Application Identifier.
+		/// </summary>
 		public ushort AppId { get; private set; }
+		/// <summary>
+		/// Indicates if the listener is active.
+		/// </summary>
 		public bool Active { get; private set; }
-		public EndPoint LocalEndpoint { get; private set; }
-		public IRudpSocket Server { get; private set; }
+		/// <summary>
+		/// Number of packets sent per second.
+		/// </summary>
 		public int SendRate { get; private set; }
+		/// <summary>
+		/// Gets the local endpoint.
+		/// </summary>
+		public EndPoint LocalEndpoint { get; private set; }
 
+		/// <summary>
+		/// Contains the bound socket.
+		/// </summary>
+		private IRudpSocket _socket;
+		/// <summary>
+		/// Communication handling thread.
+		/// </summary>
 		private Thread _listenerThread;
+		/// <summary>
+		/// Handles timing between sent packets.
+		/// </summary>
 		private Stopwatch _sendStopwatch;
+		/// <summary>
+		/// Contains all connected remote host through this listener.
+		/// </summary>
 		private Dictionary<IPEndPoint, RudpClient> _connectedClients = new Dictionary<IPEndPoint, RudpClient>();
+		/// <summary>
+		/// Remote host connection request list.
+		/// </summary>
 		private Queue<IPEndPoint> _pendingRequests;
+		/// <summary>
+		/// List of accepted remote host connections to be replied.
+		/// </summary>
 		private List<(IPEndPoint, Packet)> _acceptReplyList = new List<(IPEndPoint, Packet)>();
 		
+		/// <summary>
+		/// Creates a new RudpListener instance.
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <param name="port">socket port.</param>
+		/// <param name="sendRate">packet send rate per second.</param>
 		public RudpListener(ushort appId, int port, int sendRate) : this(appId, new IPEndPoint(IPAddress.Any, port), sendRate) { }
-		
+
+		/// <summary>
+		/// Creates a new RudpListener instance.
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <param name="address">IP address to bind to.</param>
+		/// <param name="port">socket port.</param>
+		/// <param name="sendRate">packet send rate per second.</param>
 		public RudpListener(ushort appId, IPAddress address, int port, int sendRate) : this(appId, new IPEndPoint(address, port), sendRate) { }
 
+		/// <summary>
+		/// Creates a new RudpListener instance.
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <param name="endPoint">endpoint to bind to.</param>
+		/// <param name="sendRate">packet send rate per second.</param>
 		public RudpListener(ushort appId, IPEndPoint endPoint, int sendRate)
 		{
 			AppId = appId;
@@ -38,6 +90,10 @@ namespace RUDP
 			SendRate = sendRate;
 		}
 
+		/// <summary>
+		/// Accepts the first connection request from the pending connection requests.
+		/// </summary>
+		/// <returns>New instance of RudpClient representing the remote host.</returns>
 		public RudpClient AcceptClient()
 		{
 			IPEndPoint endPoint = null;
@@ -78,6 +134,10 @@ namespace RUDP
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Indicates if there are connection requests pending.
+		/// </summary>
+		/// <returns>true if there is at leat one connection request pending.</returns>
 		public bool Pending()
 		{
 			if (!Active)
@@ -89,11 +149,18 @@ namespace RUDP
 			}			
 		}
 
+		/// <summary>
+		/// Start listening for connection requests.
+		/// </summary>
 		public void Start()
 		{
 			Start(0);
 		}
 
+		/// <summary>
+		/// Start listening for connection requests.
+		/// </summary>
+		/// <param name="backlog">limit of pending connection requests.</param>
 		public void Start(int backlog)
 		{
 			if (backlog != 0)
@@ -107,6 +174,9 @@ namespace RUDP
 			_listenerThread.Start();
 		}
 
+		/// <summary>
+		/// Stops listener and drops connected clients.
+		/// </summary>
 		public void Stop()
 		{
 			Active = false;
@@ -120,10 +190,13 @@ namespace RUDP
 			}
 		}
 
+		/// <summary>
+		/// Implements communication logic in for server mode and listener. 
+		/// </summary>
 		private void ListenerThread()
 		{
-			Server = new RudpInternalSocket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-			Server.Bind(LocalEndpoint);
+			_socket = new RudpInternalSocket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			_socket.Bind(LocalEndpoint);
 
 			_sendStopwatch = new Stopwatch();
 			_sendStopwatch.Start();
@@ -132,10 +205,10 @@ namespace RUDP
 
 			while(Active)
 			{
-				if(Server.Available > 0)
+				if(_socket.Available > 0)
 				{
 					EndPoint remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
-					int receiveCount = Server.ReceiveFrom(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, ref remoteEndpoint);
+					int receiveCount = _socket.ReceiveFrom(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, ref remoteEndpoint);
 
 					Packet packet = new Packet(receiveBuffer, 0, receiveCount);
 					if(!_connectedClients.ContainsKey((IPEndPoint)remoteEndpoint))
@@ -149,7 +222,7 @@ namespace RUDP
 										lock (_pendingRequests)
 											_pendingRequests.Enqueue((IPEndPoint)remoteEndpoint);
 									else
-										Server.SendTo(
+										_socket.SendTo(
 											new Packet()
 											{
 												AppId = AppId,
@@ -180,7 +253,7 @@ namespace RUDP
 						_sendStopwatch.Restart();
 
 						foreach ((IPEndPoint endpoint, Packet packet) reply in _acceptReplyList)
-							Server.SendTo(reply.packet.ToBytes(), reply.endpoint);
+							_socket.SendTo(reply.packet.ToBytes(), reply.endpoint);
 
 						List<IPEndPoint> disconnectedClients = new List<IPEndPoint>();
 						foreach (var pair in _connectedClients)
@@ -190,7 +263,7 @@ namespace RUDP
 							if (!pair.Value.Connected)
 								disconnectedClients.Add(pair.Key);
 
-							Server.SendTo(packet.ToBytes(), pair.Key);
+							_socket.SendTo(packet.ToBytes(), pair.Key);
 						}
 						foreach (IPEndPoint endPoint in disconnectedClients)
 							_connectedClients.Remove(endPoint);
